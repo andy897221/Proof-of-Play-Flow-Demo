b48b6f1b036233592e36ce8658d619fc40a848fb
Andy Yuen <andy897221.work@gmail.com>
2018-12-03 04:12:24 +0800

    gitignore & blockchain small update

--- PoPblockchain.py
+++ PoPblockchain.py
@@ -1,13 +1,18 @@
 import hashlib
 import json
 from time import time
+import time
 from urllib.parse import urlparse
 from uuid import uuid4
 from multiprocessing import Process
+import argparse
 
 import requests
 from flask import Flask, jsonify, request
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-b", "--bootstrapIP", type=str, help="ip address of bootstrap node")
+args = parser.parse_args()
 
 class Blockchain:
     # longest chain wins because player wants their data asap, so they will agree
@@ -38,7 +43,7 @@ class Blockchain:
     def new_block(self, plyrProof, previous_hash=None):
         block = {
             'index': len(self.chain) + 1,
-            'timestamp': time(),
+            'timestamp': time.time(),
             'matches': self.current_matches,
             'gameData': self.currernt_game_data,
             'plyrProof': plyrProof, # proof that the block writer is indeed the lottery winner
@@ -58,6 +63,7 @@ class Blockchain:
         return self.last_block['index'] + 1
 
     def valid_chain(self, chain):
+        # ALSO CHECK IF EACH BLOCK HAS ENOUGH RECORDS!!!!!!!!
         last_block = chain[0]
         current_index = 1
 
@@ -112,9 +118,16 @@ class Blockchain:
 
 
 
+myPort = 9999
+bootstrapNode = args.bootstrapIP
 app = Flask(__name__)
 node_identifier = str(uuid4()).replace('-', '')
 blockchain = Blockchain()
+
+
+@app.route('/status', methods=['GET'])
+def return_status():
+    return json.dumps({'status': 'ok'}), 200
 
 @app.route('/matches/new', methods=['POST'])
 def new_match():
@@ -125,7 +138,7 @@ def new_match():
         return 'Missing values', 400
     index = blockchain.new_match(match)
     response = {'message': f'match will be added to Block {index}'}
-    return jsonify(response), 201
+    return json.dumps(response), 201
 
 @app.route('/chain', methods=['GET'])
 def full_chain():
@@ -133,7 +146,7 @@ def full_chain():
         'chain': blockchain.chain,
         'length': len(blockchain.chain),
     }
-    return jsonify(response), 200
+    return json.dumps(response), 200
 
 @app.route('/nodes/register', methods=['POST'])
 def register_nodes():
@@ -143,11 +156,22 @@ def register_nodes():
         return "Error: Please supply a valid list of nodes", 400
     for node in nodes:
         blockchain.register_node(node)
+
+    knownNode = open('knownNodes.appData', 'w')
+    serialize_node = ""
+    for node in blockchain.nodes: serialize_node += f"{node} "
+    knownNode.write(serialize_node[:-1])
+    knownNode.close()
+
     response = {
         'message': 'New nodes have been added',
         'total_nodes': list(blockchain.nodes),
     }
-    return jsonify(response), 201
+    return json.dumps(response), 201
+
+@app.route('/nodes/retrieve', methods=['GET'])
+def return_nodes():
+    return json.dumps({"nodes", list(blockchain.nodes)}), 200
 
 @app.route('/nodes/resolve', methods=['GET'])
 def consensus():
@@ -162,16 +186,39 @@ def consensus():
             'message': 'Our chain is authoritative',
             'chain': blockchain.chain
         }
-    return jsonify(response), 200
+    return json.dumps(response), 200
 
 def run_server():
-    app.run(host='0.0.0.0', port=9999)
+    app.run(host='0.0.0.0', port=myPort)
 
 def proof_of_play():
     return
 
+def load_nodes():
+    curStatusCode = requests.codes.im_a_teapot
+    while curStatusCode != requests.codes.ok:
+        try:
+            curStatusCode = requests.get(f'127.0.0.1:{myPort}/status').status_code
+        except Exception:
+            pass
+        time.sleep(1)
+    with open('knownNodes.appData', 'r') as content:
+        for addr in content.read().split(" "): blockchain.nodes.add(addr)
+    print(f"nodes {blockchain.nodes} has been added.")
+    requested_nodes = json.loads(requests.get(bootstrapNode).text)['nodes']
+    curStatusCode = requests.codes.im_a_teapot
+    while curStatusCode != requests.codes.created:
+        try:
+            curStatusCode = request.post(f'127.0.0.1{myPort}/register_nodes',
+                data = {'nodes': requested_nodes})
+        except Exception:
+            pass
+        time.sleep(1)
+    return
+
 def main():
     Process(target=run_server, args=()).start()
+    Process(target=load_nodes, args=()).start()
     Process(target=proof_of_play, args=()).start()
 
 if __name__ == "__main__":
